ae8c5bb7e162b1d92855796ff9d4aa5c497f5363
Junior Gabi <juniorgabi@juniors-air.home>
2022-11-14 16:33:53 +0000

    got the first test to pass, now I must handle errors

--- models/topics.js
+++ models/topics.js
@@ -4,9 +4,9 @@ const pool = require("../db/connection")
 
 
 exports.selectTopic = () =>{
-
     return pool.query('SELECT * FROM topics;').then((data)=>{
-        data.rows;
+        console.log(data.rows)
+        return data.rows;
     })
 }
 
